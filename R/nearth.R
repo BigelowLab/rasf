2e6bcd5549b3a109d1efca18948b59dc16ec72c8
btupper <btupper@bigelow.org>
2017-09-15 14:38:16 -0400

    add get_boundaries()

--- R/nearth.R
+++ R/nearth.R
@@ -30,12 +30,44 @@ celmap_bb <- function(
 }
 
 
+#' Get a Spatial vector object by state or province name
+#'
+#' 'Quebec' can be used in lieu of "Qu\xe9bec"
+#' @export
+#' @param where names the states and or provinces, or group names such as 'GOM'
+#' @return a Spatial* object
+get_boundaries = function(
+    where = c("Maine", "New Brunswick", "Quebec", "New Hampshire"), 
+    what = c('boundary50m', 'boundary10m')[1]){
+    
+    
+    wh = switch(tolower(where[1]),
+        'gom' = c('Rhode Island', 'Massachusetts', 'New Hampshire',
+            'Vermont', 'Quebec', 'New Brunswick', 'Nova Scotia' , 'Maine'),
+        where)
+        
+    ix = tolower(wh) %in% 'quebec'
+    if (any(ix)) wh[ix] = "Qu\xe9bec"
+    
+    
+    what = switch(tolower(what[1]),
+        'boundary50m' = 'ne_50m_admin_1_states_provinces_shp',
+        'boundary10m' = 'ne_10m_admin_1_states_provinces_shp',
+        tolower(what[1]))
+    
+    x = nearth::read_nearth(what, what = 'vector')[[1]]
+    
+    x[x@data$name %in% where,]
+}
+     
 #' Get a Spatial vector object by region name and vector set.
 #' 
-#' Currently draws from Natural Earth
+#' Currently draws from Natural Earth.  Note that this clips the region
+#' so edge effects appear - especially for boundaries.  See \code{get_boundary()}
 #'
 #' @export
 #' @param where the name of the region
+#' @param what the type of boundary (10m, 50m etc)
 #' @param ... further arguments for \code{celmap_bb()}
 #' @return a Spatial* object
 get_vectors = function(where = c("World", "Maine", "GOM", "NWA", "NEAC")[1], 
@@ -50,5 +82,5 @@ get_vectors = function(where = c("World", "Maine", "GOM", "NWA", "NEAC")[1],
         
     bb = celmap_bb(where[1], ...)
     
-    nearth::read_nearth(what,what = 'vector', bb = bb)
+    nearth::read_nearth(what,what = 'vector', bb = bb)[[1]]
 }
