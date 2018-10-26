66bf437e4eaebc80144a86b4f84a277cf1b91db4
btupper <btupper@bigelow.org>
2018-10-26 18:07:18 -0400

    add hexbin capabilities

--- R/sp.R
+++ R/sp.R
@@ -3,18 +3,20 @@
 #' @export
 #' @param name the name of the projection string (just one)
 #' @return a projection string
-get_proj_string = function(name = c('longlat', "lcc", "utm-19", "all")[1]){
+get_proj_string = function(name = c('longlat', "lcc", "utm-19", "EPSG:3857", "all")[1]){
 
     PROJ = c(
-        longlat =  "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0",
+        "EPSG:3857" = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs',
+        "longlat" =  "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0",
         "utm-19" = "+proj=utm +zone=19 +datum=NAD83 +units=m +no_defs",
-        lcc = "+proj=lcc +lat_1=25 +lat_0=25 +lon_0=-95 +k_0=1 +x_0=0 +y_0=0 +a=6367470.21484375 +b=6367470.21484375 +units=km +no_defs")
+        "lcc" = "+proj=lcc +lat_1=25 +lat_0=25 +lon_0=-95 +k_0=1 +x_0=0 +y_0=0 +a=6367470.21484375 +b=6367470.21484375 +units=km +no_defs")
 
     switch(tolower(name[1]),
         'longlat' = PROJ[['longlat']],
         'lonlat' = PROJ[['longlat']],
         "utm-19" = PROJ[['utm-19']],
         'lcc' = PROJ[['lcc']],
+        "epsg:3857" = PROJ[["EPSG:3857"]],
         "all" = PROJ,
         paste('projection name not known:', name[1]))
 }
